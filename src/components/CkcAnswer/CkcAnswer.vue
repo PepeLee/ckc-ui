bd3dc8e75dec8df91e41a12ef95754fbe47f202e
PepeLee <15295793612@163.com>
2026-04-24 10:04:33 +0800

    init

--- src/components/CkcAnswer/CkcAnswer.vue
+++ src/components/CkcAnswer/CkcAnswer.vue
@@ -12,24 +12,24 @@
   import { computed, watch } from 'vue';
   import type { CkcAnswerProps } from '../types/ckc-answer-props';
   import { MarkdownRender } from 'markstream-vue';
-  import type { Message } from '../types/message';
+  // import type { Message } from '../types/message';
   const prop = defineProps<CkcAnswerProps>();
 
   const messageString = computed(() => {
     const content = prop.messages.map(m => m.content).join('');
     return content;
   });
-  const currentMeassageViewInfo = [
-    {
-      isExpand: false,
-      massageGroupInfo: [
-      ]
-    }
-  ]
-  const handleData = (message :Message) => {
-    currentMeassageViewInfo[currentMeassageViewInfo.length - 1].massageGroupInfo;
+  // const currentMeassageViewInfo = [
+  //   {
+  //     isExpand: false,
+  //     massageGroupInfo: [
+  //     ]
+  //   }
+  // ]
+  // const handleData = (message :Message) => {
+  //   currentMeassageViewInfo[currentMeassageViewInfo.length - 1].massageGroupInfo;
     
-  }
+  // }
   watch(() => prop.messages.length, (val) => {
     console.log('prop.messages.length:', val);
   }, { immediate: true });
